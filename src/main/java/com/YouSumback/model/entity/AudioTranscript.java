48609798905b170455c212345a8622bab85ee2b4
cheeze37 <poker97.123.321.111.223@gmail.com>
2025-05-23 11:38:35 +0900

    디렉토리 수정4

--- src/main/java/com/YouSumback/model/entity/AudioTranscript.java
+++ src/main/java/com/YouSumback/model/entity/AudioTranscript.java
@@ -7,16 +7,17 @@ import java.time.LocalDateTime;
 @Entity
 public class AudioTranscript {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "transcript_id", nullable = false)
-    private long transcript_id; // 음성텍스트 식별자
+    private long transcriptId; // 음성텍스트 식별자
 
     @Column(name = "transcript_text", columnDefinition = "TEXT", nullable = false)
-    private String transcript_text; // 추출된 음성 텍스트
+    private String transcriptText; // 추출된 음성 텍스트
 
     @Column(name = "create_at", nullable = false)
-    private LocalDateTime create_at; // 생성 일자
+    private LocalDateTime createAt; // 생성 일자
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "video_id", nullable = false)
     private Video video; // 비디오 식별자
 }
