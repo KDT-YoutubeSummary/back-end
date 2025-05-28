b897b073c4b7a204a476ed1477b9b58aaef7afe9
G1Kimdev <g1kimdev@gmail.com>
2025-05-28 14:23:59 +0900

    [Feat] 리마인더 CRUD 구현

--- src/main/java/com/YouSumback/service/ReminderService.java
+++ src/main/java/com/YouSumback/service/ReminderService.java
@@ -1,13 +1,58 @@
 package com.YouSumback.service;
 
+import com.YouSumback.model.entity.Reminder;
 import com.YouSumback.repository.ReminderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReminderService {
     @Autowired
-    ReminderRepository reminderRepository;
+    private ReminderRepository reminderRepository;
 
+    // 리마인더 등록
+    public Reminder createReminder(
+            Long userId,
+            Long userLibraryId,
+            String reminderType,
+            int frequencyInterval,
+            int dayOfWeek,
+            int dayOfMonth,
+            String baseDatetimeForRecurrence,
+            String nextNotificationDatetime,
+            String reminderNot
+    ) {
+        return reminderRepository.createReminder(userId, userLibraryId, reminderType, frequencyInterval,
+                dayOfWeek, dayOfMonth, baseDatetimeForRecurrence, nextNotificationDatetime, reminderNot);
+    }
+
+    // 사용자로 리마인더 조회
+    public List<Reminder> getRemindersByUser(Long userId) {
+        return reminderRepository.findByUser(userId);
+    }
+
+    // 리마인더 수정
+    public Reminder updateReminder(
+            Long reminderId,
+            Long userId,
+            Long userLibraryId,
+            String reminderType,
+            int frequencyInterval,
+            int dayOfWeek,
+            int dayOfMonth,
+            String baseDatetimeForRecurrence,
+            String nextNotificationDatetime,
+            String reminderNot
+    ) {
+        return reminderRepository.updateReminder(reminderId, userId, userLibraryId, reminderType, frequencyInterval,
+                dayOfWeek, dayOfMonth, baseDatetimeForRecurrence, nextNotificationDatetime, reminderNot);
+    }
+
+    // 리마인더 삭제
+    public void deleteReminder(Long reminderId) {
+        reminderRepository.deleteById(reminderId);
+    }
 
 }
