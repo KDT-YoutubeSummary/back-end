c4a8a5a788212e8503108c15c35682d1913e1d80
YeeunSeok <yes3641@naver.com>
2025-06-05 15:21:43 +0900

    refactor: UserLibraryRequestDTO.java 수정 - 토큰으로 사용자 식별 -> 클라이언트에서 받을 필요 없어서 삭제

--- src/main/java/com/kdt/yts/YouSumback/model/dto/request/UserLibraryRequestDTO.java
+++ src/main/java/com/kdt/yts/YouSumback/model/dto/request/UserLibraryRequestDTO.java
@@ -9,7 +9,8 @@ import java.util.List;
 @Setter
 // UserLibraryRequestDTO는 사용자가 요약을 라이브러리에 추가할 때 필요한 정보를 담는 DTO입니다.
 public class UserLibraryRequestDTO {
-    private Long userId;
+//    private Long userId;
+    // 사용자 ID는 인증 정보에서 가져오므로 DTO에는 포함하지 않습니다.
     private Long summaryId;
     private String userNotes;
 
