3814544629faad7a01abd0280f0b698c8b1882dc
YeeunSeok <yes3641@naver.com>
2025-06-21 00:20:20 +0900

    refactor: 오타 수정

--- src/main/java/com/kdt/yts/YouSumback/service/YouTubeMetadataService.java
+++ src/main/java/com/kdt/yts/YouSumback/service/YouTubeMetadataService.java
@@ -32,8 +32,8 @@ public class YouTubeMetadataService {
     public String extractYoutubeId(String url) {
         if (url.contains("v=")) {
             return url.substring(url.indexOf("v=") + 2).split("&")[0];
-        } else if (url.contains("youtu.be/")) {
-            return url.substring(url.indexOf("youtu.be/") + 9).split("\\?")[0];
+        } else if (url.contains("youtube/")) {
+            return url.substring(url.indexOf("youtube/") + 9).split("\\?")[0];
         }
         throw new IllegalArgumentException("유효하지 않은 유튜브 링크입니다.");
     }
